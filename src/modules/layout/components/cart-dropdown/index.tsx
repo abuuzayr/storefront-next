5328c58cb0c3dcf1f750fec27bfcc77b38b31e6f
Abu Uzayr <abu.uzayr@builtforfifty.com>
2022-08-21 09:52:57 +0800

    Add border to cart popover

--- src/modules/layout/components/cart-dropdown/index.tsx
+++ src/modules/layout/components/cart-dropdown/index.tsx
@@ -46,7 +46,7 @@ const CartDropdown = () => {
         >
           <Popover.Panel
             static
-            className="hidden small:block absolute top-[calc(100%+1px)] right-0 bg-white border-x border-b border-gray-200 w-[382px] text-gray-900"
+            className="hidden small:block absolute top-[calc(100%+5px)] right-0 bg-white border border-gray-200 w-[382px] text-gray-900 z-50"
           >
             <div className="p-4 flex items-center justify-center">
               <h3 className="text-large-semi">Shopping Bag</h3>
