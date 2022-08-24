9b8d18bbd58bc70fa7c072fc97692e87cea5168c
Abu Uzayr <abu.uzayr@builtforfifty.com>
2022-08-25 01:50:47 +0800

    Add singular effect component

---
+++ src/modules/common/components/effect/index.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react"
+import { Heading, Stack, Text } from "@chakra-ui/react"
+import contentfulClient from "@lib/util/contentful-client"
+import Image from "next/image"
+
+const Effect = ({ id }: { id: string }) => {
+  const [effect, setEffect] = useState<{ [x: string]: any }>()
+
+  useEffect(() => {
+    async function getEffect() {
+      const response = await contentfulClient.getEntry(id)
+      console.log(response.fields)
+      setEffect(response.fields)
+    }
+    getEffect()
+  }, [id])
+
+  return (
+    <>
+      {effect && (
+        <Stack>
+          <Image
+            alt={effect.image.fields.title}
+            src={`https:${effect.image.fields.file.url}`}
+            height={120}
+            width={120}
+          />
+          <Heading as="h3" fontSize={21} color="brand.400" fontWeight="400">
+            {effect.title}
+          </Heading>
+          <Text size="sm" maxW="2xl" mt={4}>
+            {effect.subtitle}
+          </Text>
+        </Stack>
+      )}
+    </>
+  )
+}
+
+export default Effect
