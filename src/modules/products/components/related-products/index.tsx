914b0c7ed59faa9d51751e52f7d58745128b51f3
Abu Uzayr <abu.uzayr@builtforfifty.com>
2022-08-25 01:51:39 +0800

    Alter related products to our use case

--- src/modules/products/components/related-products/index.tsx
+++ src/modules/products/components/related-products/index.tsx
@@ -1,100 +1,89 @@
-import { fetchProductsList } from "@lib/data"
-import usePreviews from "@lib/hooks/use-previews"
-import getNumberOfSkeletons from "@lib/util/get-number-of-skeletons"
-import repeat from "@lib/util/repeat"
-import { Product, StoreGetProductsParams } from "@medusajs/medusa"
-import Button from "@modules/common/components/button"
-import SkeletonProductPreview from "@modules/skeletons/components/skeleton-product-preview"
-import { useCart } from "medusa-react"
-import { useMemo } from "react"
-import { useInfiniteQuery } from "react-query"
-import ProductPreview from "../product-preview"
+import Image from "next/image"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import {
+  Heading,
+  Text,
+  Container,
+  Box,
+  SimpleGrid,
+} from "@chakra-ui/react"
+import contentfulClient from "@lib/util/contentful-client"
 
 type RelatedProductsProps = {
-  product: Product
+  data: {[key: string]: any}
 }
 
-const RelatedProducts = ({ product }: RelatedProductsProps) => {
-  const { cart } = useCart()
+const RelatedProducts = ({ data }: RelatedProductsProps) => {
+  const [products, setProducts] = useState<Array<any>>([])
 
-  const queryParams: StoreGetProductsParams = useMemo(() => {
-    const params: StoreGetProductsParams = {}
-
-    if (cart?.id) {
-      params.cart_id = cart.id
+  useEffect(() => {
+    async function getProducts() {
+      const response = await contentfulClient.getEntries({
+        content_type: "product",
+        "fields.medusaId[in]": data.products.map((product: any) => product.sys.id).join(","),
+      })
+      setProducts(response.items)
     }
-
-    if (product.collection_id) {
-      params.collection_id = [product.collection_id]
-    }
-
-    if (product.type) {
-      params.type = product.type.id
-    }
-
-    if (product.tags) {
-      params.tags = product.tags.map((t) => t.value)
-    }
-
-    params.is_giftcard = false
-
-    return params
-  }, [product, cart?.id])
-
-  const { data, hasNextPage, fetchNextPage, isLoading, isFetchingNextPage } =
-    useInfiniteQuery(
-      [`infinite-products-${product.id}`, queryParams, cart],
-      ({ pageParam }) => fetchProductsList({ pageParam, queryParams }),
-      {
-        getNextPageParam: (lastPage) => lastPage.nextPage,
-      }
-    )
-
-  const previews = usePreviews({ pages: data?.pages, region: cart?.region })
+    if (data.products.length) getProducts()
+  }, [data.products])
 
   return (
-    <div className="product-page-constraint">
-      <div className="flex flex-col items-center text-center mb-16">
-        <span className="text-base-regular text-gray-600 mb-6">
-          Related products
-        </span>
-        <p className="text-2xl-regular text-gray-900 max-w-lg">
-          You might also want to check out these products.
-        </p>
-      </div>
-
-      <ul className="grid grid-cols-2 small:grid-cols-3 medium:grid-cols-4 gap-x-4 gap-y-8">
-        {previews.map((p) => (
-          <li key={p.id}>
-            <ProductPreview {...p} />
-          </li>
-        ))}
-        {isLoading &&
-          !previews.length &&
-          repeat(8).map((index) => (
-            <li key={index}>
-              <SkeletonProductPreview />
-            </li>
-          ))}
-        {isFetchingNextPage &&
-          repeat(getNumberOfSkeletons(data?.pages)).map((index) => (
-            <li key={index}>
-              <SkeletonProductPreview />
-            </li>
-          ))}
-      </ul>
-      {hasNextPage && (
-        <div className="flex items-center justify-center mt-8">
-          <Button
-            isLoading={isLoading}
-            onClick={() => fetchNextPage()}
-            className="w-72"
-          >
-            Load more
-          </Button>
-        </div>
-      )}
-    </div>
+    <Container centerContent textAlign="center" my={10} maxW="8xl">
+      <Heading as="h3" fontSize={42} color="brand.400" fontWeight="400">
+        你或許會喜歡
+      </Heading>
+      <SimpleGrid columns={products.length < 4 ? products.length : 4}>
+      {products
+        .map(({ fields }) => {
+          let height = 0
+          let width = 0
+          if (fields.thumbnail) {
+            height = fields.thumbnail.fields.file.details.image.height
+            width = fields.thumbnail.fields.file.details.image.width
+            if (height > 300) {
+              width = (300 / height) * width
+              height = 300
+            }
+          }
+          return (
+            <Link
+              href={`/products/${fields.handle}`}
+              key={fields.handle}
+              passHref
+            >
+              <Box p={10}>
+                {fields.thumbnail && (
+                  <Image
+                    alt={fields.thumbnail.fields.title}
+                    key={fields.thumbnail.fields.title}
+                    style={{
+                      maxHeight: 300,
+                    }}
+                    width={width}
+                    height={height}
+                    src={`https:${fields.thumbnail.fields.file.url}`}
+                  />
+                )}
+                <Text size="sm" mt={4}>
+                  {fields.title}
+                </Text>
+                {fields.variants.length ? (
+                  <Text size="sm" mt={2}>
+                    {fields.variants[0].fields.prices[0] &&
+                      fields.variants[0].fields.prices[0].currency_code.toUpperCase()}
+                    {fields.variants[0].fields.prices[0] &&
+                      fields.variants[0].fields.prices[0].amount}
+                  </Text>
+                ) : (
+                  <></>
+                )}
+              </Box>
+            </Link>
+          )
+        })}
+      </SimpleGrid>
+    </Container>
   )
 }
 
