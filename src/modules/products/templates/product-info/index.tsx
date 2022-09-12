755a43378321fbb94914d80f3ceb47aa79fb84f8
Abu Uzayr <abu.uzayr@builtforfifty.com>
2022-09-13 01:50:48 +0800

    Handle possibility of product or bundle

--- src/modules/products/templates/product-info/index.tsx
+++ src/modules/products/templates/product-info/index.tsx
@@ -1,17 +1,20 @@
 import ProductActions from "@modules/products/components/product-actions"
+import BundleActions from "@modules/products/components/bundle-actions"
 import React from "react"
 import { Product } from "types/medusa"
 
 type ProductInfoProps = {
-  product: Product
+  product?: Product
+  bundle?: { [x: string]: any }
 }
 
-const ProductInfo: React.FC<ProductInfoProps> = ({ product }) => {
+const ProductInfo: React.FC<ProductInfoProps> = ({ product, bundle }) => {
   return (
     <div id="product-info">
       <div className="flex flex-col gap-y-12 lg:max-w-[500px] mx-auto">
         <div>
-          <ProductActions product={product} />
+          {product && <ProductActions product={product} />}
+          {bundle && <BundleActions bundle={bundle} />}
         </div>
       </div>
     </div>
