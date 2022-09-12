a2468c33ccf8cc7a2c159f4cec7a8fec0d1a7648
Abu Uzayr <abu.uzayr@builtforfifty.com>
2022-09-13 01:46:38 +0800

    Add handle for bundle

---
+++ src/pages/bundles/[handle].tsx
@@ -0,0 +1,116 @@
+import { medusaClient } from "@lib/config"
+import { IS_BROWSER } from "@lib/constants"
+import { getProductHandles } from "@lib/util/get-product-handles"
+import Head from "@modules/common/components/head"
+import Layout from "@modules/layout/templates"
+import ProductBundleTemplate from "@modules/products/templates/bundles"
+import SkeletonProductPage from "@modules/skeletons/templates/skeleton-product-page"
+import { GetStaticPaths, GetStaticProps } from "next"
+import { useRouter } from "next/router"
+import { ParsedUrlQuery } from "querystring"
+import { ReactElement } from "react"
+import { dehydrate, QueryClient, useQuery } from "react-query"
+import { NextPageWithLayout, PrefetchedPageProps } from "types/global"
+import contentfulClient from "@lib/util/contentful-client"
+
+interface Params extends ParsedUrlQuery {
+  handle: string
+}
+
+const fetchBundle = async (handle: string) => {
+  const response = await contentfulClient.getEntries({
+    content_type: "bundle",
+    limit: 1,
+    "fields.handle": handle,
+  })
+  console.log(response)
+  return response.items[0].fields
+}
+
+const ProductPage: NextPageWithLayout<PrefetchedPageProps> = ({ notFound }) => {
+  const { query, isFallback, replace } = useRouter()
+  const handle = typeof query.handle === "string" ? query.handle : ""
+  console.log(handle)
+
+  const { data, isError, isLoading, isSuccess } = useQuery(
+    [`get_bundle`, handle],
+    () => fetchBundle(handle),
+    {
+      enabled: handle.length > 0,
+      keepPreviousData: true,
+    }
+  )
+
+  if (notFound) {
+    if (IS_BROWSER) {
+      replace("/404")
+    }
+
+    return <SkeletonProductPage />
+  }
+
+  if (isFallback || isLoading || !data) {
+    return <SkeletonProductPage />
+  }
+
+  if (isError) {
+    replace("/404")
+  }
+
+  if (isSuccess) {
+    console.log(data)
+    return (
+      <>
+        <Head
+          description={data.description}
+          title={data.name}
+          image={data.thumbnail}
+        />
+        <ProductBundleTemplate data={data} />
+      </>
+    )
+  }
+
+  return <></>
+}
+
+ProductPage.getLayout = (page: ReactElement) => {
+  return <Layout>{page}</Layout>
+}
+
+export const getStaticPaths: GetStaticPaths<Params> = async () => {
+  const handles = await getProductHandles()
+  return {
+    paths: handles.map((handle) => ({ params: { handle } })),
+    fallback: true,
+  }
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const handle = context.params?.handle as string
+
+  const queryClient = new QueryClient()
+
+  await queryClient.prefetchQuery([`get_product`, handle], () =>
+    fetchBundle(handle)
+  )
+
+  const queryData = await queryClient.getQueryData([`get_product`, handle])
+
+  if (!queryData) {
+    return {
+      props: {
+        notFound: true,
+      },
+    }
+  }
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+      notFound: false,
+    },
+  }
+}
+
+export default ProductPage
