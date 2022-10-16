5c857f92f96121d1cf7a92730501b16c2fb17d36
Abu Uzayr <abu.uzayr@builtforfifty.com>
2022-10-16 08:47:23 +0800

    Remove padding

--- src/pages/store.tsx
+++ src/pages/store.tsx
@@ -12,7 +12,7 @@ const Store: NextPageWithLayout = () => {
   return (
     <>
       <Head title="Store" description="Explore all of our products." />
-      <div className="flex flex-col small:flex-row small:items-start py-6">
+      <div className="content-container">
         <RefinementList refinementList={params} setRefinementList={setParams} />
         <InfiniteProducts params={params} />
       </div>
