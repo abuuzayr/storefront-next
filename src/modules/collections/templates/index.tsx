af5021274b3f95819016e6c5c3e23c4e41c8c82f
Abu Uzayr <abu.uzayr@builtforfifty.com>
2022-10-18 09:36:19 +0800

    Add sorting

--- src/modules/collections/templates/index.tsx
+++ src/modules/collections/templates/index.tsx
@@ -5,24 +5,29 @@ import ProductPreview from "@modules/products/components/product-preview"
 import SkeletonProductPreview from "@modules/skeletons/components/skeleton-product-preview"
 import { fetchCollectionProducts } from "@pages/collections/[handle]"
 import { useCart } from "medusa-react"
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { useInView } from "react-intersection-observer"
 import { useInfiniteQuery } from "react-query"
 import { GoSettings } from "react-icons/go"
-import { Button, Menu, MenuButton, MenuItem, MenuList, Text } from "@chakra-ui/react"
+import { Button, Flex, Icon, Menu, MenuButton, MenuItem, MenuList, Text } from "@chakra-ui/react"
+import { BsCheck } from "react-icons/bs"
 
 type CollectionTemplateProps = {
   collection: {
     id: string
     title: string
   }
+  count: number | null
 }
 
 const CollectionTemplate: React.FC<CollectionTemplateProps> = ({
   collection,
+  count
 }) => {
   const { cart } = useCart()
   const { ref, inView } = useInView()
+  const [sortBy, setSortBy] = useState("")
+  const [sortDir, setSortDir] = useState("")
 
   const {
     data: infiniteData,
@@ -43,10 +48,37 @@ const CollectionTemplate: React.FC<CollectionTemplateProps> = ({
     }
   )
 
-  const previews = usePreviews({
+  let previews = usePreviews({
     pages: infiniteData?.pages,
     region: cart?.region,
   })
+
+  if (sortBy && sortDir) {
+    if (sortBy === 'price') {
+      previews.sort((a, b) => {
+        const priceA: number = a.price?.raw || 0
+        const priceB: number = b.price?.raw || 0
+        if (priceA === priceB) return 0
+        if (!priceA && !priceB) return 0
+        if (!priceA && priceB) return -1
+        if (!priceB && priceA) return 1
+        if (sortDir === 'asc') return priceA - priceB
+        return priceB - priceA
+      })
+    }
+    if (sortBy === 'date') {
+      previews.sort((a, b) => {
+        const dateA: number = new Date(a?.createdAt || new Date()).getTime()
+        const dateB: number = new Date(b?.createdAt || new Date()).getTime()
+        if (dateA === dateB) return 0
+        if (!dateA && !dateB) return 0
+        if (!dateA && dateB) return -1
+        if (!dateB && dateA) return 1
+        if (sortDir === 'asc') return dateA - dateB
+        return dateB - dateA
+      })
+    }
+  }
 
   useEffect(() => {
     if (inView && hasNextPage) {
@@ -64,8 +96,8 @@ const CollectionTemplate: React.FC<CollectionTemplateProps> = ({
         >
           {collection.title}
         </h1>
-        <div className="ml-auto">
-          <Menu>
+        <Flex direction="column" ml="auto" gap={0}>
+          <Menu closeOnSelect={false}>
             <MenuButton
               as={Button}
               variant="ghost"
@@ -84,13 +116,26 @@ const CollectionTemplate: React.FC<CollectionTemplateProps> = ({
               </div>
             </MenuButton>
             <MenuList>
-              <MenuItem>上架時間: 由新到舊</MenuItem>
-              <MenuItem>上架時間: 由舊到新</MenuItem>
-              <MenuItem>價格: 由高至低</MenuItem>
-              <MenuItem>價格: 由低至高</MenuItem>
+              <MenuItem onClick={() => {
+                setSortBy("date")
+                setSortDir("desc")
+              }}>上架時間: 由新到舊 {sortBy === "date" && sortDir === "desc" && <Icon as={BsCheck} w={6} h={6} color="brand.400" />}</MenuItem>
+              <MenuItem onClick={() => {
+                setSortBy("date")
+                setSortDir("asc")
+              }}>上架時間: 由舊到新 {sortBy === "date" && sortDir === "asc" && <Icon as={BsCheck} w={6} h={6} color="brand.400" />}</MenuItem>
+              <MenuItem onClick={() => {
+                setSortBy("price")
+                setSortDir("desc")
+              }}>價格: 由高至低 {sortBy === "price" && sortDir === "desc" && <Icon as={BsCheck} w={6} h={6} color="brand.400" />}</MenuItem>
+              <MenuItem onClick={() => {
+                setSortBy("price")
+                setSortDir("asc")
+              }}>價格: 由低至高 {sortBy === "price" && sortDir === "asc" && <Icon as={BsCheck} w={6} h={6} color="brand.400" />}</MenuItem>
             </MenuList>
           </Menu>
-        </div>
+          {count !== null && <Text size="sm" color="gray.500">{count} 產品</Text>}
+        </Flex>
       </div>
       <ul className="grid grid-cols-2 small:grid-cols-3 medium:grid-cols-4 gap-x-4 gap-y-8">
         {previews.map((p) => (
